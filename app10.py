260f0c06f1b3d8fe9a33991b8ac4c46cca345c07
Ethan <champ9162@naver.com>
2022-06-13 19:05:52 +0900

    Update app10.py

--- app10.py
+++ app10.py
@@ -1,8 +1,34 @@
 from tkinter import *
-import pickle
+from tkinter import messagebox
 
 def clearlistbox():
     listb.delete(0, "end")
+
+def deleteall():
+    message = messagebox.askquestion(
+        '삭제 경고', '정말로 리스트를 비우겠습니까?')
+    print(message)
+    if message.upper() == "YES":
+        global day_list, time_list, place_list, people_list, task_list, sunny_list, rain_list, hazy_list, snow_list, wind_list, thun_list, verysati_list, sati_list, neither_list, dissati_list, verydissati_list,felt_list
+        day_list= []
+        time_list = []
+        place_list = []
+        people_list = []
+        task_list = []
+        sunny_list = []
+        rain_list = []
+        hazy_list = []
+        snow_list = []
+        wind_list = []
+        thun_list = []
+        verysati_list = []
+        sati_list = []
+        neither_list = []
+        dissati_list = []
+        verydissati_list = []
+        felt_list = []
+        update()
+
 
 def update():
     clearlistbox()
@@ -57,6 +83,8 @@ def update():
     for felt in felt_list:
         listb.insert("end", felt)
 
+    return
+
 def deleteone():
     delt = listb.get("active")
     if delt in day_list:
@@ -104,11 +132,11 @@ def deleteone():
     update()
 
     if delt in verysati_list:
-        verysati_list.remove(delt)
+        sati_list.remove(delt)
     update()
 
     if delt in sati_list:
-        sati_list.remove(delt)
+        task_list.remove(delt)
     update()
 
     if delt in neither_list:
@@ -120,13 +148,14 @@ def deleteone():
     update()
 
     if delt in verydissati_list:
-        verydissati_list.remove(delt)
+        verysati_list.remove(delt)
     update()
 
     if delt in felt_list:
         felt_list.remove(delt)
     update()
 
+    return
 
 def day_in():
     lbl_1_display["text"] = ""
@@ -277,13 +306,13 @@ def verydissati_in():
     return verydissati
 
 def felt_in():
-    lbl_felt_display["text"] = ""
+    lbl_felt["text"] = ""
     felt = felt_input.get()
     if felt != "":
         felt_list.append(felt)
         update()
     else:
-        lbl_felt_display["text"] = "할 일이 없어? 게으르게 살거야?"
+        lbl_felt["text"] = "할 일이 없어? 게으르게 살거야?"
     felt_input.delete(0, 'end')
 
 def add():
@@ -316,36 +345,40 @@ def add():
                  "\n" \
                  "느낀점\n" \
                  "\n" \
-                 "{} ".format(day, weather, time, people, people, place, task, sati, felt)
+                 "{} ".format(day,weather,time,people, people, place, task, sati, felt)
     print(diary_text)
 
-    lbl_diary_2['text'] = ''
-    if diary_text != "":
-        lbl_diary_2['text'] = diary_text
-        update()
+    message = messagebox.askquestion(
+        '일기 쓰기', '리스트를 확정하여 일기를 불러오시겠습니까?')
+    print(message)
+    if message.upper() == "YES":
+        lbl_diary_2['text'] = ''
+        if diary_text != "":
+            lbl_diary_2['text'] = diary_text
+            update()
 
-    with open("diary.pickle", "wb") as f:
-        pickle.dump(diary_text, f)
+    f = open('diary_text.txt', 'w')
+    f.write(diary_text)
 
-    return diary_text
+    return
 
 window = Tk()
 window.title("일기를 써주세요")
 window.configure(bg='white')
-window.geometry("1000x555")
-window.resizable(False, False)
+window.geometry("1000x600+100+100")
+window.resizable(True, True)
 
 day_list = []
-time_list = []
-place_list = []
-people_list = []
-task_list = []
 sunny_list = []
 rain_list = []
 hazy_list = []
 snow_list = []
 wind_list = []
 thun_list = []
+time_list = []
+place_list = []
+people_list = []
+task_list = []
 verysati_list = []
 sati_list = []
 neither_list = []
@@ -434,56 +467,53 @@ btn_task.place(x=240, y=150)
 lbl_6_display = Label(window, text="", bg="white")
 lbl_6_display.place(x=290, y=150)
 
-# 만족도
-lbl_satisfaction = Label(window, text="만족도", bg="white", width=5)
-lbl_satisfaction.place(x=30, y=180)
-
-btn_verysati = Button(master=window, text="매우만족", width=7, command=verysati_in)
-btn_verysati.place(x=80, y=180)
-
-btn_sati = Button(master=window, text="만족", width=7, command=sati_in)
-btn_sati.place(x=145, y=180)
-
-btn_neither = Button(master=window, text="보통", width=7, command=neither_in)
-btn_neither.place(x=210, y=180)
-
-btn_dissati = Button(master=window, text="나쁨", width=7, command=dissati_in)
-btn_dissati.place(x=275, y=180)
-
-btn_verydissati = Button(master=window, text="매우나쁨", width=7, command=verydissati_in)
-btn_verydissati.place(x=340, y=180)
-
-lbl_satisfaction_display = Label(window, text="", bg="white")
-lbl_satisfaction_display.place(x=400, y=180)
-
-#느낀점
-lbl_felt = Label(window, text="느낀점", bg="white", width=5)
-lbl_felt.place(x=30, y=215)
-
-felt_input = Entry(window, width=45)
-felt_input.place(x=80, y=215, height=130)
-
-btn_add = Button(window, text="추가", bg="white", width=5, command=felt_in)
-btn_add.place(x=353, y=215)
-
-lbl_felt_display = Label(window, text="", bg="white")
-lbl_felt_display.place(x=80, y=355)
-
 # 저장
 btn_update = Button(window, text="완료", width=5, command=add)
-btn_update.place(x=335, y=380, height=50)
+btn_update.place(x=305, y=200, height=50)
 
-btn_del = Button(window, text="삭제", width=5, command=deleteone)
-btn_del.place(x=335, y=445, height=35)
+btn_del = Button(window, text="삭제", width=5, command= deleteone)
+btn_del.place(x=305, y=265, height=35)
 
-btn_diary = Button(window, text="clear", width=5, command=clearlistbox)
-btn_diary.place(x=335, y=495, height=35)
+btn_diary = Button(window, text="clear", width=5, command=deleteall)
+btn_diary.place(x=305, y=315, height=35)
 
 listb = Listbox()
-listb.place(x=30, y=380, width=300, height=150)
+listb.place(x=0, y=200, width=300, height=150)
+
+# 만족도
+lbl_satisfaction = Label(window, text="만족도", bg="white", width=5)
+lbl_satisfaction.place(x=30, y=380)
+
+btn_verysati = Button(master=window, text="매우만족", width=7, command=verysati_in)
+btn_verysati.place(x=80, y=380)
+
+btn_sati = Button(master=window, text="만족", width=7, command=sati_in)
+btn_sati.place(x=145, y=380)
+
+btn_neither = Button(master=window, text="보통", width=7, command=neither_in)
+btn_neither.place(x=210, y=380)
+
+btn_dissati = Button(master=window, text="나쁨", width=7, command=dissati_in)
+btn_dissati.place(x=275, y=380)
+
+btn_verydissati = Button(master=window, text="매우나쁨", width=7, command=verydissati_in)
+btn_verydissati.place(x=340, y=380)
+
+lbl_satisfaction_display = Label(window, text="", bg="white")
+lbl_satisfaction_display.place(x=400, y=380)
+
+#느낀점
+lbl_felt = Label(window, text="느낀점", bg="white",width=5)
+lbl_felt.place(x=30, y=410)
+
+felt_input = Entry(window, width=45)
+felt_input.place(x=80, y=410, height=130)
+
+btn_add = Button(window, text="추가", bg="white", width=5,command=felt_in)
+btn_add.place(x=353, y=410)
 
 # 일기
-lbl_diary = Label(window, text="일기", bg="white", width=5)
+lbl_diary = Label(window, text="일기장", bg="white", width=5)
 lbl_diary.place(x=500, y=0)
 lbl_diary_2 = Label(window, bg="white", relief="solid")
 lbl_diary_2.place(x=500, y=30, width=480, height=500)
